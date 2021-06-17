2482940676f762e967aa01eec5010ff3a8206234
Daniel Paulus <daniel.paulus@icloud.com>
2021-06-17 17:20:33 +0200

    split qrcode generator into own module

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,15 @@
-
-
 import * as fs from 'fs';
 
-import QRCode from 'qrcode'
-
 import { initGUI } from './guiLoader'
-
+import { generateQR } from './qrCodeGenerator'
 
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
 import express from 'express';
 
-import * as os from 'os'
-
-const en0 = os.networkInterfaces()["en0"]
-if (en0 === undefined) {
-  throw "no network interface en0 found"
-}
-const ipAddress = en0.filter(x => x.family === "IPv4")[0].address
-const port = 8000
-const generateQR = async (text: string) => {
-  try {
-    await QRCode.toFile("temp/qr.png", text)
-  } catch (err) {
-    console.error(err)
-  }
-}
 (async () => {
-  await generateQR(`http://${ipAddress}:${port}`)
-
-  const absoulteImagePath = 'temp/qr.png';
+  const port = 8000
+  const absoulteImagePath = await generateQR(port)
   let headless = false
   var myArgs = process.argv.slice(2);
   if (myArgs.length > 0) {
