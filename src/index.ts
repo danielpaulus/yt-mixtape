622d82a5c507a2c681273e974e82bb73f0fe4075
danielpaulus <daniel.paulus@icloud.com>
2021-06-17 21:55:21 +0200

    Add restapi (#2)
    * create class for mediainfo, add rest api endpoint
    
    * put webserver in different file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import * as fs from 'fs';
+
 
 import { initGUI } from './guiLoader'
 import { generateQR } from './qrCodeGenerator'
+import { configureExpress } from './webserver'
 
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
 import express from 'express';
+import {MediaInfo} from './mediainfo'
 
 (async () => {
   const port = 8000
@@ -24,21 +26,8 @@ import express from 'express';
 
   // rest of the code remains same
   const app = express();
-
-  app.use(express.static('public'));
-  app.get('/', (req, res) => {
-    const files: any[] = [];
-    fs.readdirSync('public/media').forEach(file => {
-      if (file.endsWith(".webm")) {
-        const info = JSON.parse(fs.readFileSync("public/media/" + file.replace(".webm", ".json")).toString())
-
-        files.push({ filename: file, title: info.title });
-      }
-
-    });
-    const fileLinks: string = files.map(x => `<a href="video.html?v=${x.filename}">${x.title}</a>`).join("<br/>")
-    res.send(fileLinks)
-  });
+configureExpress(app);
+  
   app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
   });
