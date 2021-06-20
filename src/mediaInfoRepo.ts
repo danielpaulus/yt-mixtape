18c580c9df41ead9e945ae83764850a609060639
Daniel Paulus <daniel.paulus@icloud.com>
2021-06-20 08:42:09 +0200

    added unit test for database insert and list, added jest

--- src/mediaInfoRepo.ts
+++ src/mediaInfoRepo.ts
@@ -1,20 +1,37 @@
 
 import sqlite3 from 'sqlite3';
 import {open, Database as GDatabase} from 'sqlite';
+import {MediaInfo} from './mediainfo';
 
 export class MediaInfoRepository {
   db: Promise<GDatabase<sqlite3.Database, sqlite3.Statement>>;
-  constructor() {
+  constructor(dbFile: string) {
     this.db = open({
-      filename: 'database.db',
+      filename: dbFile,
       driver: sqlite3.Database,
     });
   }
-  async dothings() {
+  async initDb(): Promise<void> {
     const db = await this.db;
-    return db.run(`CREATE TABLE todo(
-        task text
-      );`);
+    await db.run(`CREATE TABLE IF NOT EXISTS mediainfo (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        video_id TEXT,
+        title TEXT,
+        uploaded_by TEXT)`);
+  }
+
+  async list():Promise<MediaInfo[]> {
+    const dbset = await (await this.db).all(`SELECT * from mediainfo`);
+    const result:MediaInfo[] = [];
+    dbset.forEach(function(value) {
+      result.push(new MediaInfo(value.title, value.uploaded_by, value.video_id));
+    });
+    return result;
+  }
+
+  async add(m:MediaInfo) :Promise<void> {
+    await (await this.db).run(`INSERT INTO mediainfo (video_id, title, uploaded_by)
+      VALUES (?, ?, ?)`, [m.id, m.title, m.uploadedBy]);
   }
 }
 
---
+++ src/mediaInfoRepo.unit.test.ts
@@ -0,0 +1,30 @@
+
+import * as fs from 'fs';
+import {MediaInfo} from './mediainfo';
+import {MediaInfoRepository} from './mediaInfoRepo';
+
+let repo:MediaInfoRepository;
+
+beforeEach(async ()=>{
+  repo = new MediaInfoRepository('test.db');
+  await repo.initDb();
+});
+
+it('initializes the db only once', async ()=>{
+  await repo.initDb();
+  await repo.initDb();
+});
+it('can insert an element', async ()=>{
+  const firstList = await repo.list();
+  expect(firstList).toStrictEqual([]);
+
+  const info = new MediaInfo('test_title', 'dan', 'someID');
+  await repo.add(info);
+
+  const secondList = await repo.list();
+  expect(secondList).toStrictEqual([info]);
+});
+
+afterEach(()=>{
+  fs.promises.unlink('test.db');
+});
